76a84de310b4861156c48469364570ad802e33c2
Jonathan Kung <jkung2314@gmail.com>
2018-02-21 22:30:39 -0800

    Fixed syntax

--- credentials.py
+++ credentials.py
@@ -2,12 +2,12 @@
 
 UCSC_LDAP_SERVER = ''
 UCSC_LDAP_DN = ''
-UCSC_LDAP_FIELDS = 
+UCSC_LDAP_FIELDS = ''
 UCSC_LDAP_BIND_DN = ""
 
 SOE_LDAP_SERVER = ''
 SOE_LDAP_DN = ''
-SOE_LDAP_FIELDS = 
+SOE_LDAP_FIELDS = ''
 
 # How long to wait before performing the next LDAP query or bind
 # Probably not an issue for small batches, larger batches we may want to consider being nicer
