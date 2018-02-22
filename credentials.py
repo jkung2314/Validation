f58cec7799a59130d86c5ab284d9ade4b29273a1
Jonathan Kung <jkung2314@gmail.com>
2018-02-21 22:21:24 -0800

    Create credentials.py ... previously hidden

---
+++ credentials.py
@@ -0,0 +1,15 @@
+
+
+UCSC_LDAP_SERVER = ''
+UCSC_LDAP_DN = ''
+UCSC_LDAP_FIELDS = 
+UCSC_LDAP_BIND_DN = ""
+
+SOE_LDAP_SERVER = ''
+SOE_LDAP_DN = ''
+SOE_LDAP_FIELDS = 
+
+# How long to wait before performing the next LDAP query or bind
+# Probably not an issue for small batches, larger batches we may want to consider being nicer
+# to the ldap server. I have run a 0.1 delay against 5000 lines without issue or complaint from IDM.
+LDAP_ACTION_DELAY = 0.1
